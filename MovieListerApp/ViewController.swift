fd76ab5485e097ef8dc60eb983ca3e955bf475e5
Umurcan Kocaman <umurcan.kocaman@bilyoner.com>
2020-06-14 12:27:51 +0300

    Init Project

---
+++ MovieListerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieListerApp
+//
+//  Created by Umurcan Kocaman on 14.06.2020.
+//  Copyright © 2020 ukocaman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
