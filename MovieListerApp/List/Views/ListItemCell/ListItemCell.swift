ec057bb204223f19cc8ac5f909bae54b7ebcd36c
Umurcan Kocaman <kocaman.umurcan@gmail.com>
2020-06-14 13:41:43 +0300

    Add Movie List View Controller Design

---
+++ MovieListerApp/List/Views/ListItemCell/ListItemCell.swift
@@ -0,0 +1,28 @@
+//
+//  ListItemCell.swift
+//  MovieListerApp
+//
+//  Created by Umurcan Kocaman on 14.06.2020.
+//  Copyright © 2020 ukocaman. All rights reserved.
+//
+
+import UIKit
+
+class ListItemCell: UITableViewCell {
+
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var labelType: UILabel!
+    @IBOutlet weak var labelYear: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ MovieListerApp/List/Views/ListViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ListViewController.swift
+//  MovieListerApp
+//
+//  Created by Umurcan Kocaman on 14.06.2020.
+//  Copyright © 2020 ukocaman. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupTableView()
+    }
+    
+    func setupTableView() {
+        tableView.registerNib(name: "ListItemCell")
+    }
+}
+
+extension ListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListItemCell", for: indexPath) as! ListItemCell
+        return cell
+    }
+    
+    
+}
