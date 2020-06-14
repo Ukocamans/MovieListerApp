0ab2a99f6237a748120d2892d1075a7d39374443
Umurcan Kocaman <kocaman.umurcan@gmail.com>
2020-06-14 14:11:01 +0300

    Add SplashViewController design

---
+++ MovieListerApp/Splash/Views/SplashViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SplashViewController.swift
+//  MovieListerApp
+//
+//  Created by Umurcan Kocaman on 14.06.2020.
+//  Copyright © 2020 ukocaman. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    @IBOutlet weak var labelTitle: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+//            let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+//            let viewController = mainStoryboard.instantiateViewController(withIdentifier: "MainNavigationController") as! UINavigationController
+//            UIApplication.shared.keyWindow?.rootViewController = viewController
+//        }
+        
+    }
+}
