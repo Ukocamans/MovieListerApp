872cc10b50e38f32ee0648cbbb80872219c0f186
Umurcan Kocaman <kocaman.umurcan@gmail.com>
2020-06-14 13:59:55 +0300

    Add Detail View Controller Design

---
+++ MovieListerApp/Detail/Views/DetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailViewController.swift
+//  MovieListerApp
+//
+//  Created by Umurcan Kocaman on 14.06.2020.
+//  Copyright © 2020 ukocaman. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var imageViewPoster: UIImageView!
+    @IBOutlet weak var labelGenre: UILabel!
+    @IBOutlet weak var labelPlot: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
