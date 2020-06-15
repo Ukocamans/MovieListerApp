8ead01f7a04b87f5fd914e1ffd1676b0ace7cce7
Umurcan Kocaman <kocaman.umurcan@gmail.com>
2020-06-15 18:41:40 +0300

    Change project deployment target Fix table view ui in list

--- MovieListerApp/List/Views/ListViewController.swift
+++ MovieListerApp/List/Views/ListViewController.swift
@@ -24,6 +24,7 @@ class ListViewController: UIViewController {
     
     func setupTableView() {
         tableView.registerNib(name: "ListItemCell")
+        tableView.tableFooterView = UIView()
     }
     
     func setupRx() {
